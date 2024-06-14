5cb116bfaf24e0289ddd3e0fc8408e9edbcd3825
Yash Hegde <yash4success@gmail.com>
2024-06-14 17:20:23 +0530

    works with file storage pics as well

--- public/fr-scripts.js
+++ public/fr-scripts.js
@@ -8,15 +8,15 @@ const run = async () => {
         faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
         faceapi.nets.ageGenderNet.loadFromUri('./models'),
     ])
-    let refFace = await faceapi.fetchImage('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Michael_Jordan_in_2014.jpg/220px-Michael_Jordan_in_2014.jpg');
+    let refFace = await faceapi.fetchImage('./images/yash-single.JPG');
     
-    let FacesToCheck = await faceapi.fetchImage('https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/JordanSmithWorthy2.jpg/170px-JordanSmithWorthy2.jpg');
+    let FacesToCheck = await faceapi.fetchImage('./images/yash_with_manjeet.jpg');
     
     // let faceOne = await faceapi.fetchImage('image_url'); //check this step => how the canvas is being drawn on the image in the DOM, if we are fetching the image , then that fetched object, and the DOM object are 2 different things right ?
     let reffaceData = await faceapi.detectAllFaces(refFace).withFaceLandmarks().withFaceDescriptors();
     let facesToCheckData = await faceapi.detectAllFaces(FacesToCheck).withFaceLandmarks().withFaceDescriptors();
-
-    //console.log(faceData)
+    
+    console.log(facesToCheckData)
 
     //grabbing the image:
     const canvas = document.getElementById("canvas");
@@ -25,6 +25,7 @@ const run = async () => {
         //face matching:
     let faceMatcher = new faceapi.FaceMatcher(reffaceData)
     facesToCheckData = faceapi.resizeResults(facesToCheckData, FacesToCheck)
+
     
     //loop thru all of the faces in our imageToCheck and compare to our reference face data
     facesToCheckData.forEach(face => {
@@ -35,7 +36,7 @@ const run = async () => {
         if (label.includes("unknown")) {
             return
         }
-        let options = { label: "Jordan" }
+        let options = { label: "Yash" }
         const drawBox = new faceapi.draw.DrawBox(detection.box, options);
         drawBox.draw(canvas)
     });
